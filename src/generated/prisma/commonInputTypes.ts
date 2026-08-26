1452e1f5c0a1befa173525d56e7842af50f80be6
yeasinrafee <mdyeasinrafi7@gmail.com>
2026-08-26 18:24:03 +0600

    Seed script fixed

--- src/generated/prisma/commonInputTypes.ts
+++ src/generated/prisma/commonInputTypes.ts
@@ -10,8 +10,8 @@
  */
 
 import type * as runtime from "@prisma/client/runtime/client"
-import * as $Enums from "./enums.ts"
-import type * as Prisma from "./internal/prismaNamespace.ts"
+import * as $Enums from "./enums.js"
+import type * as Prisma from "./internal/prismaNamespace.js"
 
 
 export type StringFilter<$PrismaModel = never> = {
